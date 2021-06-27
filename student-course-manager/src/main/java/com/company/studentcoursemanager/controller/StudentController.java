11ad4c8297d3e8a70f4bc754b5b49e2ecb6f7400
Dennis Francis <dnfrancis22@gmail.com>
2021-06-26 22:25:00 -0400

    updated endpoint

--- student-course-manager/src/main/java/com/company/studentcoursemanager/controller/StudentController.java
+++ student-course-manager/src/main/java/com/company/studentcoursemanager/controller/StudentController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/students")
 public class StudentController {
@@ -17,6 +19,13 @@ public class StudentController {
     @ResponseStatus(HttpStatus.CREATED)
     public Student createStudent(@RequestBody Student student) {
         return studentRepo.save(student);
+    }
+
+    @GetMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Student getStudentById(@PathVariable Integer id) {
+        Optional<Student> student = studentRepo.findById(id);
+        return student.get();
     }
 
     @PutMapping(value ="/{id}")
